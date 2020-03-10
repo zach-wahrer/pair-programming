c2019786a43949ac32e8f3f04551827f53d1a763
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-10 16:27:34 -0600

    Make function return instead of using a var

--- shortest_path_out_of_maze.py
+++ shortest_path_out_of_maze.py
@@ -16,29 +16,36 @@ board = [
 # Brute Force solution
 def steps_out(board: list) -> int:
 
+    log = {}
+
+    def _logger(key):
+        if key not in log:
+            log[key] = 0
+            print("New key")
+        else:
+            log[key] += 1
+            print(key, "count", log[key])
+
     def _inbounds(row, col):
         return row in range(0, len(board)) and col in range(0, len(board[0]))
 
     def _on_exit(row, col):
         return row == len(board) - 1 and col == len(board[0]) - 1
 
-    possible_paths = []
-
-    def _traverse_board(row, col, count, possible_paths):
-
+    def _traverse_board(row, col, count):
+        _logger((row, col))
         if _inbounds(row, col):
             if board[row][col] == 0:
                 if _on_exit(row, col):
-                    possible_paths.append(count)
-                else:
-                    _traverse_board(row + 1, col, count + 1, possible_paths)
-                    _traverse_board(row, col + 1, count + 1, possible_paths)
+                    return count
 
-    _traverse_board(0, 0, 0, possible_paths)
+                return min(_traverse_board(row + 1, col, count + 1),
+                           _traverse_board(row, col + 1, count + 1))
 
-    if possible_paths:
-        return min(possible_paths)
-    return -1
+        return float('inf')
+
+    moves = _traverse_board(0, 0, 0)
+    return -1 if type(moves) == float else moves
 
 
 print(steps_out(board))
